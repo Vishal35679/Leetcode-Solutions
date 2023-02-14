2ee7cec81a8301613fc4d2cb8d27adb74d1481b1
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-02-14 12:35:47 +0530

    67. Add Binary

---
+++ LC - 67.cpp
@@ -0,0 +1,53 @@
+67. Add Binary
+
+CODE:
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        if(a.size()>b.size()){
+            while(b.size()<a.size()){
+                b+="0";
+            }
+        }
+        else if(b.size()>a.size()){
+            while(a.size()<b.size()){
+                a+="0";
+            }
+        }
+        string ans="";
+        int c=0;
+        for(int i=0;i<a.size();i++){
+            if(a[i]=='1' && b[i]=='1' && c==0){
+                ans+="0";
+                c=1;
+            }
+            else if(a[i]=='1' && b[i]=='1' && c==1){
+                ans+="1";
+                c=1;
+            }
+            else if(((a[i]=='1' && b[i]=='0') || (a[i]=='0' && b[i]=='1')) && c==0){
+                ans+="1";
+                c=0;
+            }
+            else if(((a[i]=='1' && b[i]=='0') || (a[i]=='0' && b[i]=='1')) && c==1){
+                ans+="0";
+                c=1;
+            }
+            else if(a[i]=='0' && b[i]=='0' && c==0){
+                ans+="0";
+                c=0;
+            }
+            else if(a[i]=='0' && a[i]=='0' && c==1){
+                ans+="1";
+                c=0;
+            }
+        }
+        if(c==1){
+            ans+="1";
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
