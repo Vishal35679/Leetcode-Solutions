913a4f026db0a2df594f69136c2e02af2c4f4938
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-04-03 19:45:41 +0530

    881. Boats to Save People

---
+++ LC - 881.cpp
@@ -0,0 +1,27 @@
+881. Boats to Save People
+
+CODE:
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int boatCount = 0;
+        sort(people.begin(), people.end());
+        
+        int left = 0;
+        int right = people.size() - 1;
+        
+        while(left <= right){
+            int sum = people[left] + people[right];
+            if(sum <= limit){
+                boatCount++;
+                left++;
+                right--;
+            }
+            else{
+                boatCount++;
+                right--;
+            }
+        }
+        return boatCount;
+    }
+};
