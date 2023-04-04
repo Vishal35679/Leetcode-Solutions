bef2cdb2a5f982cec603c9e0fdb20b7864e13fdd
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-04-04 23:49:38 +0530

    2405. Optimal Partition of String

---
+++ LC - 2405.cpp
@@ -0,0 +1,28 @@
+2405. Optimal Partition of String
+
+CODE:
+class Solution {
+public:
+    int partitionString(string s) {
+        int n = s.length();
+        if(n==1)
+            return 1;
+        
+        int ans = 0;
+        set<char>freq; 
+        freq.insert(s[0]);
+
+        for(int i=1; i<n ;i++){
+            if(freq.find(s[i]) == freq.end() )
+                freq.insert(s[i]);
+            
+            else
+            {
+                ans++;
+                freq.clear();
+                freq.insert(s[i]);
+            }
+        }
+        return ans+1;
+    }
+};
