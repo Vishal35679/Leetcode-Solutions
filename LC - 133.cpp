b325170b0c87749a364277538ac6920e95a346cb
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-04-08 21:56:35 +0530

    133. Clone Graph

---
+++ LC - 133.cpp
@@ -0,0 +1,32 @@
+133. Clone Graph
+
+CODE:
+class Solution {
+public:
+    unordered_map<Node* , Node*> mp; 
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) {
+            return NULL;
+        }
+        Node* first = new Node(node -> val, {}); 
+        mp[node] = first;
+        queue<Node*> q; 
+        q.push(node); 
+        while(q.empty() == false) 
+        {
+            Node* curr = q.front(); 
+            q.pop(); 
+            
+            for(auto adj: curr -> neighbors) 
+            {
+                if(mp.find(adj) == mp.end()) 
+                {
+                    mp[adj] = new Node(adj -> val, {}); 
+                    q.push(adj);   
+                }
+                mp[curr] -> neighbors.push_back(mp[adj]); 
+            }
+        }
+        return mp[node];
+    }
+};
