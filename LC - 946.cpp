7bf4e4a9c796437730f197afa7f3d859c681bc80
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-04-22 21:48:27 +0530

    946. Validate Stack Sequences

---
+++ LC - 946.cpp
@@ -0,0 +1,18 @@
+946. Validate Stack Sequences
+
+CODE:
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int l = 0;
+        for (int i : pushed) {
+            st.push(i);
+            while (!st.empty() && st.top() == popped[l]) {
+                st.pop();
+                l++;
+            }
+        }
+        return st.empty();
+    }
+};
