36e327f3988733e1b5af62a8e3a74ac15a334b28
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-06-14 07:56:05 +0530

    530. Minimum Absolute Difference in BST

---
+++ LC - 530.cpp
@@ -0,0 +1,17 @@
+530. Minimum Absolute Difference in BST
+
+CODE:
+class Solution {
+public:
+    int min_dif = INT_MAX, val = -1;    
+int getMinimumDifference(TreeNode* root) {
+    if (root->left != NULL) 
+        getMinimumDifference(root->left);
+    if (val >= 0) 
+        min_dif = min(min_dif, root->val - val);
+    val = root->val;
+    if (root->right != NULL) 
+        getMinimumDifference(root->right);
+    return min_dif;
+}
+};
