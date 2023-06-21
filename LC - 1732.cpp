579ff9a5c5e9f330a8febccbc4df6fa60ecfd627
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-06-21 21:50:46 +0530

    1732. Find the Highest Altitude

---
+++ LC - 1732.cpp
@@ -0,0 +1,15 @@
+1732. Find the Highest Altitude
+
+CODE:
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int max_alt=0;
+        int curr_alt=0;
+        for(int i=0;i<gain.size();i++){
+            curr_alt+=gain[i]; 
+            max_alt=max(curr_alt, max_alt);
+        }
+        return max_alt;
+    }
+};
