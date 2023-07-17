4390551e1b5001e3d052142023bd027f16f6c23e
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-07-18 00:21:40 +0530

    231. Power of Two

--- LC - 231.cpp
+++ LC - 231.cpp
@@ -4,6 +4,18 @@ CODE:
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        while(n%2==0) {
+            n=n/2;
+        }
+        return (n==1);
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
         for(int i=0; i<=30; i++)
         {
             int ans=pow(2,i);
