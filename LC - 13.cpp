ae6f1a2e7c4faec80e26661db869a0a03d61f58c
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-07-18 00:17:19 +0530

    13. Roman to Integer

---
+++ LC - 13.cpp
@@ -0,0 +1,26 @@
+13. Roman to Integer
+
+CODE:
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans=0,prev=0,num=0;
+        for(int i=s.length()-1;i>=0;i--){
+            switch(s[i]){
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+            if(num<prev)
+                ans-=num;
+            else
+                ans+=num;
+            prev=num;
+        }
+        return ans;
+    }
+};
