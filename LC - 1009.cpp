c6e235d436e0114fdff7338f8db340d4204b8919
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-07-18 00:19:41 +0530

    1009. Complement of Base 10 Integer

--- LC - 1009.cpp
+++ LC - 1009.cpp
@@ -1,5 +1,28 @@
 1009. Complement of Base 10 Integer
+    
 CODE:
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        vector<int> temp;
+        int res=0;
+        while(n!=0) {
+            temp.push_back(n%2);
+            n/=2;
+        }
+        for(int i=0; i<temp.size(); i++) {
+            if(temp[i]==0) temp[i]=1;
+            else temp[i]=0;
+        }
+        for(int i=temp.size()-1; i>-1; i--) {
+            res = res*2 + temp[i];
+        }
+        return res;
+    }
+};
+
+//bit manipulation
 class Solution {
 public:
     int bitwiseComplement(int n) {
